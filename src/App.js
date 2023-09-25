efbf6cec6d7054855ee6e2c4afaccf28886744ec
luigi <virtual.employee@gmail.com>
2023-09-25 02:54:30 -0600

    Monsters Rolodex from Udemy course completed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
+import SearchBox from './components/search-box/search-box.component.jsx';
+import CardList from './components/card-list/card-list.component.jsx';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return {
+            monsters: users,
+            searchField: '',
+          };
+        })
+      );
+  }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    // [{ name:'Leanne'}, {name:'Yihua'}]
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className='App'>
+        <h1 className='app-title'> Monster`s Rolodex</h1>
+        <SearchBox
+          className='search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='search monsters'
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
 export default App;
